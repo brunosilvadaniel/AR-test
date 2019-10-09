74c4016e2a614091b975a739d8e8f6dee64f2409
brunosilvadaniel <42727140+brunosilvadaniel@users.noreply.github.com>
2019-10-09 14:16:51 -0300

    Add files via upload

---
+++ js/Click_mouse.js
@@ -0,0 +1,16 @@
+<script>
+  AFRAME.registerComponent('navigate-on-click', {
+    schema: {
+      url: {default: ''}
+    },
+
+    init: function () {
+      var data = this.data;
+      var el = this.el;
+
+      el.addEventListener('click', function () {
+        window.location.href = data.url;
+      });
+    }
+  });        
+</script>
