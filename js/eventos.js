69819a970a344d6971055dbe0ecf33bd4afb45c4
brunosilvadaniel <42727140+brunosilvadaniel@users.noreply.github.com>
2019-10-21 14:11:17 -0300

    Update eventos.js

--- js/eventos.js
+++ js/eventos.js
@@ -4,13 +4,10 @@ AFRAME.registerComponent('markerhandler', {
         const animatedMarker = document.querySelector("#animated-marker");
         const aEntity = document.querySelector("#animated-model");
 
-        // every click, we make our model grow in size :)
-        animatedMarker.addEventListener('click', function(ev, target){
+       animatedMarker.addEventListener('click', function(ev, target){
             const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
             if (aEntity && intersectedElement === aEntity) {
-                const scale = aEntity.getAttribute('scale');
-                Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
-                aEntity.setAttribute('scale', scale);
+                window.location.href = "https://www1.satc.edu.br/portais/acesso/public/#/";
             }
         });
 }});
