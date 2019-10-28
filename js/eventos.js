ddc1167918faf78f5130b4f3c3299d5088d7b7e1
brunosilvadaniel <42727140+brunosilvadaniel@users.noreply.github.com>
2019-10-28 15:42:00 -0300

    Update eventos.js

--- js/eventos.js
+++ js/eventos.js
@@ -7,7 +7,7 @@ AFRAME.registerComponent('markerhandler', {
         animatedMarker.addEventListener('click', function(ev, target){
             const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
             if (aEntity && intersectedElement === aEntity) {
-                window.location.href = "brunosilvadaniel.github.io/Page2.html";
+                window.location.href = "/Page2.html";
             }
         });
 }});
